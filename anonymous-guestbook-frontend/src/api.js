80bed1286db36f53892195b3d73aaf993e109515
leeeeegun <leeeeegun@naver.com>
2025-07-14 16:38:08 +0900

    feat: [이의건] Front 글 수정, 삭제 기능 추가 [TEAM2-3RD-8]

--- anonymous-guestbook-frontend/src/api.js
+++ anonymous-guestbook-frontend/src/api.js
@@ -2,11 +2,19 @@ import axios from 'axios';
 
 // axios 인스턴스 생성 (공통 baseURL 설정)
 const api = axios.create({
-    baseURL: '/api/guestbook',  // 상대경로로 변경
+    baseURL: '/api/guestbook',
 });
 
 // 방명록 목록 가져오기
 export const getGuestbookList = () => api.get('');  // 빈 문자열로 호출
+
+// 글 작성
 export const postGuestbook = (data) => api.post('', data);
+
+// 글 삭제
+export const deleteGuestbook = (id) => api.delete(`/${id}`);
+
+// 글 수정
+export const updateGuestbook = (id, data) => api.put(`/${id}`, data);
 
 export default api;
